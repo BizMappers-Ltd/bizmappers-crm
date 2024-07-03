5a06ad4c4f452d1291e9983158d8c48f68bbac19
ashfiaKhatun <ashfia1204@gmail.com>
2024-07-03 18:51:13 +0600

    changed design

--- resources/views/template/home/layouts/head.blade.php
+++ resources/views/template/home/layouts/head.blade.php
@@ -5,7 +5,7 @@
 <!-- theme meta -->
 <meta name="theme-name" content="quixlab" />
 
-<title>BizMapperss</title>
+<title>BizMappers</title>
 <!-- Favicon icon -->
 <link rel="icon" type="../../template/image/png" sizes="16x16" href="../../template/images/favicon.png">
 <!-- Pignose Calender -->
